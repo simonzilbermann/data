f92d49139a4acc6341049823cfcba0ae52e512aa
simonzilbermann <106626490+simonzilbermann@users.noreply.github.com>
2025-01-09 15:29:01 +0200

    Update recolte-data.js

--- api/v1/controller/recolte-data.js
+++ api/v1/controller/recolte-data.js
@@ -5,40 +5,49 @@ const IPS = require("../model/recolte-data");
 const mongoose = require("mongoose");
 
 module.exports = {
-  save_data: async (req, res) => {
+ save_data: async (req, res) => {
     try {
-      // Récupérer l'IP à partir de la requête
       let ip = requestIp.getClientIp(req);
-      console.log("IP initiale :", ip); // Afficher l'IP récupérée
+      console.log("IP initiale :", ip);
 
-      // Si l'IP est en IPv6 locale "::1" ou "127.0.0.1", la forcer à "127.0.0.1"
       if (ip === "::1" || ip === "127.0.0.1") {
         ip = "127.0.0.1"; // Force l'adresse IPv4 locale
       }
 
-      // Si l'IP vient de l'en-tête "x-forwarded-for", l'extraire
       if (req.headers["x-forwarded-for"]) {
-        ip = req.headers["x-forwarded-for"].split(",")[0].trim(); // Prendre la première IP dans l'en-tête
-        console.log("Nouvelle IP à partir de 'x-forwarded-for' :", ip); // Afficher l'IP après extraction
+        ip = req.headers["x-forwarded-for"].split(",")[0].trim();
+        console.log("Nouvelle IP à partir de 'x-forwarded-for' :", ip);
       }
 
-      // Analyser le user-agent
       const agent = useragent.parse(req.headers["user-agent"]);
 
-      // Créer un nouvel enregistrement de données
+      // Obtenir les données de géolocalisation
+      const geoResponse = await axios.get(`http://ip-api.com/json/${ip}`);
+      const geoData = geoResponse.data;
+
+      // Vérifiez si la requête de géolocalisation est réussie
+      if (geoData.status !== "success") {
+        console.error("Erreur de géolocalisation :", geoData.message);
+        throw new Error(
+          "Impossible de récupérer les données de géolocalisation"
+        );
+      }
+
+      const { lat, lon } = geoData;
+
       const new_data = new IPS({
         _id: new mongoose.Types.ObjectId(),
-        addressIp: ip, // L'adresse IP obtenue est maintenant toujours en IPv4
+        addressIp: ip,
         browser: agent.toAgent(),
         os: agent.os.toString(),
         device: agent.device.toString(),
         date: new Date(),
+        latitude: lat, // Latitude obtenue
+        longitude: lon, // Longitude obtenue
       });
 
-      // Sauvegarder les données dans la base de données
       await new_data.save();
 
-      // Répondre avec un message de succès
       res
         .status(200)
         .send({ message: "Données sauvegardées avec succès", data: new_data });
